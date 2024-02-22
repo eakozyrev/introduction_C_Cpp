c3f4ff6d5add3c1769645f04173b03c4eca50e6c
eakozyrev <eakozyrev09@gmail.com>
2024-02-22 11:45:31 +0700

    sdsd

---
+++ 3_Gregory/Source.cpp
@@ -0,0 +1,27 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// pi = 4 - 4/3 + 4/5 - 4/7 + ...
+
+int main() {
+
+	printf("pi = %f\n", M_PI);
+	printf("pi = %.3f\n", M_PI);
+	int n;
+	scanf_s("%d",&n);
+
+	// Вычислить пи, используя 
+	// ряд Грегори
+
+	//округлять сверху
+	//3.14159  ->  3.1416
+	//3.1415   ->  3.142
+	//3.141    ->  3.14
+
+	printf("%.*f", 12, 3.143452342351561);
+
+	return 0;
+
+}
