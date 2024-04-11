2ebd88397a5f8f406a47bf43b71af59071b4fd25
eakozyrev <eakozyrev09@gmail.com>
2024-04-11 10:11:23 +0700

    10_char_start after lesson

--- 10_char_star/test.cpp
+++ 10_char_star/test.cpp
@@ -1,7 +1,6 @@
 //https://github.com/eakozyrev/introduction_C_Cpp.git
 // Compile: g++ test.cpp
-// Run:     ./a.out
-
+// Run:     ./a.out   // ./a.exe в Windows
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -38,27 +37,37 @@ char* GetString(FILE* stream, char symbol = '.')
 
 int main(){
 
-char str1[100] = "Privet, MIR!";
-printf("sizeof(str1) = %d\n", sizeof(str1));
+char str1[] = "Privet, MIR!";
 printf("str1 = %s\n",str1);
+printf("sizeof(str1) = %d\n", sizeof(str1));
 str1[6] = 0; // = '\0'
 printf("str1 = %s\n",str1);
-
+printf("sizeof(str1) = %d\n", sizeof(str1));
 // объединение строк
-strcat(str1, "!");
+char * str3 = strcat(str1, "!!!!!!!");
 printf("strcat(str1, \"!\") = %s\n",str1);
+printf("adress str1 = %p\n",str1);
+printf("sizeof(str1) = %d\n", sizeof(str1));
+printf("str3 = %s\n", str3);
+printf("sizeof(str3) = %d\n", sizeof(str3));
+printf("sizeof(str3) = %d\n", sizeof(*str3));
 
 // char * strcpy ( char * destination, const char * source );
 strcpy(str1, "Poka");
 printf("strcpy(str1, \"Poka\"): %s\n",str1);
 
+str1[0] += 'a' - 'A';
+printf("\'a\' - \'A\': %s\n",str1);
+
 // сравнение строк, равные строки вернут 0
-int compare = strcmp(str1, "Poka");
+int compare = strcmp(str1, "poka");
 printf("compare = %d\n",compare);
 
 char *str2 = GetString(stdin);
 printf("str2 = %s\n",str2);
-
+int size = 0;
+for(size = 0; *(str2+size) != '\0'; size++);
+printf("size str2 = %d\n",size);
 free(str2);
 
 return 0;
